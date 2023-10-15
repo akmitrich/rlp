0297b75d7f180e53e37af853f7ca9fa6f1ddb710
akmitrich <ak.mitrich@mail.ru>
2023-10-15 21:32:51 +0300

    Small test for parse

--- src/parse.rs
+++ src/parse.rs
@@ -19,87 +19,94 @@ pub enum PatternObject {
 }
 
 pub fn parse(re: &str) -> Vec<PatternElement> {
-    let mut i = 0;
     let mut stack = vec![vec![]];
-    let re = re.chars().collect::<Vec<_>>();
-    while i < re.len() {
-        let next = dbg!(re[i]);
+    let mut re = re.chars();
+    loop {
+        let next = if let Some(c) = re.next() {
+            c
+        } else {
+            break;
+        };
         match next {
             '.' => {
-                stack.last_mut().unwrap().push(PatternElement {
-                    quantifier: Quantifier::ExactlyOne,
-                    object: PatternObject::Wildcard,
-                });
-                i += 1;
+                stack.last_mut().unwrap().push(PatternElement::new(
+                    Quantifier::ExactlyOne,
+                    PatternObject::Wildcard,
+                ));
             }
-            '?' => {
-                if let Some(last_element) = stack.last_mut().unwrap().last_mut() {
-                    if last_element.quantifier != Quantifier::ExactlyOne {
-                        panic!("Too many quantifiers in the row");
-                    }
-                    last_element.quantifier = Quantifier::ZeroOrOne;
-                    i += 1;
-                }
-            }
-            '*' => {
-                if let Some(last_element) = stack.last_mut().unwrap().last_mut() {
-                    if last_element.quantifier != Quantifier::ExactlyOne {
-                        panic!("Too many quantifiers in the row");
-                    }
-                    last_element.quantifier = Quantifier::ZeroOrMore;
-                    i += 1;
-                }
-            }
-            '+' => {
-                if let Some(last_element) = stack.last_mut().unwrap().last_mut() {
-                    if last_element.quantifier != Quantifier::ExactlyOne {
-                        panic!("Too many quantifiers in the row");
-                    }
-                    let mut zero_or_more_copy = last_element.to_owned();
-                    zero_or_more_copy.quantifier = Quantifier::ZeroOrMore;
-                    stack.last_mut().unwrap().push(zero_or_more_copy);
-                    i += 1;
-                }
-            }
-            '(' => {
-                stack.push(vec![]);
-                i += 1;
-            }
-            ')' => {
-                if stack.len() <= 1 {
-                    panic!("Unexpected ')' token.");
-                }
-                let states = stack.pop().unwrap();
-                let group = PatternElement {
-                    quantifier: Quantifier::ExactlyOne,
-                    object: PatternObject::Group(states),
-                };
-                stack.last_mut().unwrap().push(group);
-                i += 1;
-            }
+            '?' => make_sure_exactly_one_and_update(&mut stack, Quantifier::ZeroOrOne).unwrap(),
+            '*' => make_sure_exactly_one_and_update(&mut stack, Quantifier::ZeroOrMore).unwrap(),
+            '+' => make_one_or_more(&mut stack).unwrap(),
+            '(' => stack.push(vec![]),
+            ')' => make_group(&mut stack).unwrap(),
             '\\' => {
-                if i + 1 >= re.len() {
+                if let Some(c) = re.next() {
+                    push_literal(stack.last_mut().unwrap(), c);
+                } else {
                     panic!("Bad escape character.")
                 }
-                stack.last_mut().unwrap().push(PatternElement {
-                    quantifier: Quantifier::ExactlyOne,
-                    object: PatternObject::Literal(re[i + 1]),
-                });
-                i += 2;
             }
-            c => {
-                stack.last_mut().unwrap().push(PatternElement {
-                    quantifier: Quantifier::ExactlyOne,
-                    object: PatternObject::Literal(c),
-                });
-                i += 1;
-            }
+            c => push_literal(stack.last_mut().unwrap(), c),
         }
     }
     if stack.len() != 1 {
         panic!("Unmatched groups in regular expression");
     }
     stack.first().unwrap().to_owned()
+}
+
+fn make_sure_exactly_one_and_update(
+    stack: &mut [Vec<PatternElement>],
+    quantifier: Quantifier,
+) -> Result<(), String> {
+    if let Some(last_element) = stack.last_mut().unwrap().last_mut() {
+        if last_element.quantifier != Quantifier::ExactlyOne {
+            return Err("Too many quantifiers in the row".to_owned());
+        }
+        last_element.quantifier = quantifier;
+        Ok(())
+    } else {
+        Err("No element for quantifier".to_owned())
+    }
+}
+
+fn make_one_or_more(stack: &mut [Vec<PatternElement>]) -> Result<(), String> {
+    let states = stack.last_mut().unwrap();
+    if let Some(last_element) = states.last_mut() {
+        if last_element.quantifier != Quantifier::ExactlyOne {
+            return Err("Too many quantifiers in the row".to_owned());
+        }
+        let mut zero_or_more_copy = last_element.to_owned();
+        zero_or_more_copy.quantifier = Quantifier::ZeroOrMore;
+        states.push(zero_or_more_copy);
+        Ok(())
+    } else {
+        Err("No element fo quantifier '+'".to_owned())
+    }
+}
+
+fn make_group(stack: &mut Vec<Vec<PatternElement>>) -> Result<(), String> {
+    if stack.len() <= 1 {
+        Err("Unexpected ')' token.".to_owned())
+    } else {
+        let states = stack.pop().unwrap();
+        let group = PatternElement::new(Quantifier::ExactlyOne, PatternObject::Group(states));
+        stack.last_mut().unwrap().push(group);
+        Ok(())
+    }
+}
+
+fn push_literal(states: &mut Vec<PatternElement>, c: char) {
+    states.push(PatternElement::new(
+        Quantifier::ExactlyOne,
+        PatternObject::Literal(c),
+    ));
+}
+
+impl PatternElement {
+    pub fn new(quantifier: Quantifier, object: PatternObject) -> Self {
+        Self { quantifier, object }
+    }
 }
 
 #[cfg(test)]
@@ -110,7 +117,6 @@ mod tests {
     fn it_works() {
         let regex = r"a?(b.*c)+d";
         let result = parse(regex);
-        dbg!(&result);
         assert_eq!(4, result.len());
         assert_eq!(
             &PatternElement {
